b283e262c5ffe01a93e41a7fdf41f4563c2f3fce
segunjkf <sege.timz12@gmail.com>
2022-09-05 21:44:57 +0100

    fmt

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "my-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+
   tags = {
     name = "main"
   }
@@ -9,6 +10,7 @@ resource "aws_vpc" "my-vpc" {
 resource "aws_subnet" "public0" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = "10.0.3.0/24"
+
   tags = {
     name = "public"
   }
@@ -21,9 +23,11 @@ resource "aws_subnet" "private1" {
     name = "private"
   }
 }
+
 resource "aws_subnet" "private0" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = "10.0.4.0/24"
+
   tags = {
     name = "private0"
   }
@@ -40,6 +44,7 @@ resource "aws_subnet" "public1" {
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.my-vpc.id
+
   tags = {
     name = "main"
   }
@@ -48,6 +53,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_nat_gateway" "main0" {
   allocation_id = aws_eip.nat1.id
   subnet_id     = aws_subnet.private1.id
+  
   tags = {
     name = "main0"
   }
@@ -56,6 +62,7 @@ resource "aws_nat_gateway" "main0" {
 resource "aws_nat_gateway" "main1" {
   allocation_id = aws_eip.nat0.id
   subnet_id     = aws_subnet.private0.id
+
   tags = {
     name = "main1"
   }
@@ -63,6 +70,7 @@ resource "aws_nat_gateway" "main1" {
 
 resource "aws_eip" "nat0" {
   vpc = true
+
   tags = {
     name = "main"
   }
@@ -70,10 +78,12 @@ resource "aws_eip" "nat0" {
 
 resource "aws_eip" "nat1" {
   vpc = true
+
   tags = {
     name = "main"
   }
 }
+
 
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
@@ -84,6 +94,7 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public0.id
   route_table_id = aws_route_table.public.id
 }
+
 resource "aws_route_table_association" "private0" {
   subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.private0.id
@@ -96,6 +107,7 @@ resource "aws_route_table_association" "private1" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.my-vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
@@ -108,6 +120,7 @@ resource "aws_route_table" "private0" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main0.id
   }
+
   tags = {
     name = "private0"
   }
@@ -123,7 +136,7 @@ resource "aws_route_table" "private1" {
     name = "private"
   }
 }
-
+]
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
@@ -149,6 +162,7 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   tags = {
     Name = "my-sg"
   }
