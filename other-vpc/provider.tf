af8c9aaaa8ce4c8caf6aabdc5d34b0e028ba3114
segunjkf <sege.timz12@gmail.com>
2022-09-08 09:48:47 +0100

    naming var

---
+++ other-vpc/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.1.9"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
