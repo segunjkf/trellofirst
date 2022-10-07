3df6b40a82be6a68728db1f3f5dc64dfee7069a9
segunjkf <sege.timz12@gmail.com>
2022-10-07 12:17:19 +0100

    ASG

---
+++ level2/ASG.tf
@@ -0,0 +1,31 @@
+resource "aws_launch_configuration" "main" {
+  name_prefix     = "${var.env_code}-launch-config"
+  image_id        = data.aws_ami.linux-image.id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.private.id]
+
+  user_data = file("user-data.sh")
+
+  key_name = "ansible"
+}
+
+resource "aws_autoscaling_group" "main" {
+  name             = var.env_code
+  min_size         = 2
+  max_size         = 5
+  desired_capacity = 2
+
+  target_group_arns         = [aws_lb_target_group.main.arn]
+  launch_configuration      = aws_launch_configuration.main.name
+  vpc_zone_identifier       = data.terraform_remote_state.layer1.outputs.subnet_id_private
+
+
+tag {
+    key                 = "Name"
+    value               = "${var.env_code}-asg"
+    propagate_at_launch = true
+  }
+}
+
+
+
