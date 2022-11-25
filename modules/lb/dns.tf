85982ef10c896d740f11a51b4086726a770f54df
segunjkf <sege.timz12@gmail.com>
2022-11-25 13:21:30 +0100

    drain

--- modules/lb/dns.tf
+++ modules/lb/dns.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = datws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "www.${data.aws_route53_zone.main.name}"
   type    = "CNAME"
   ttl     = 300
