446cf214d5c40b24767569075d22918dc750327b
segunjkf <sege.timz12@gmail.com>
2022-11-25 12:38:28 +0100

    domain

---
+++ modules/lb/dns.tf
@@ -0,0 +1,14 @@
+data "aws_route53_zone" "main" {
+  name = "kaytheog.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = datws_route53_zone.main.zone_id
+  name    = "www.${data.aws_route53_zone.main.name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.main-elb.dns_name]
+}
+
+
+           
